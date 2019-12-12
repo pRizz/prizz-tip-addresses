841d6134b3f7cb55defd44231195f0bbf7016942
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2019-12-11 23:17:52 -0600

    Added BTC

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   NANO: 'nano_3fwbzdazf6wmmy17agkzp47f75d5hdm5m91bxexp9auowm8m9a7tsk8prizz',
   IOTA: 'PRIZ9SWUXJLZKRPUYESORIZXVANRQUUGURDE9HXWOLNLGJOQHQSVEQYUM9GJTTAVKKTSUDWKCCBLCMAFAQARGOXPXW',
-  BANANO: 'ban_3prizz6d765kf76h5i6exqagjtpc4a4adyryfwfuwsmu9mtyaoo39n66yykq'
+  BANANO: 'ban_3prizz6d765kf76h5i6exqagjtpc4a4adyryfwfuwsmu9mtyaoo39n66yykq',
+  BTC: 'bc1qln4y04ss0nv4td8ajuwrz868htpplr5arp5hjx'
 }
