726153f7e4f6ace6864335037c5f8570a98d5f44
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2019-12-15 00:40:51 -0600

    Add eth tip address

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   NANO: 'nano_3fwbzdazf6wmmy17agkzp47f75d5hdm5m91bxexp9auowm8m9a7tsk8prizz',
   IOTA: 'PRIZ9SWUXJLZKRPUYESORIZXVANRQUUGURDE9HXWOLNLGJOQHQSVEQYUM9GJTTAVKKTSUDWKCCBLCMAFAQARGOXPXW',
   BANANO: 'ban_3prizz6d765kf76h5i6exqagjtpc4a4adyryfwfuwsmu9mtyaoo39n66yykq',
-  BTC: 'bc1qln4y04ss0nv4td8ajuwrz868htpplr5arp5hjx'
+  BTC: 'bc1qln4y04ss0nv4td8ajuwrz868htpplr5arp5hjx',
+  ETH: '0xA87d8b48DdbA2fE76504F343ca2683c89cEee595'
 }
